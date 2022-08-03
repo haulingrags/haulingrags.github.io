bbbfb03791a4c31d6912e5ce4782d4735e996a8e
haulingrags <110570897+haulingrags@users.noreply.github.com>
2022-08-03 18:19:39 -0400

    added content

---
+++ main.py
@@ -0,0 +1,71 @@
+import turtle
+t = turtle.Turtle()
+t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+for i in range(49):
+  t.forward(3.1416)
+  t.right(3.6)
+for i in range(75):
+  t.forward(3.1416)
+  t.left(3.6)
+for i in range(100):
+  t.forward(3.1416)
+  t.right(3.6)
+t.right(180)
+for i in range(75):
+  t.forward(3.1416)
+  t.right(3.6)
+for i in range(49):
+  t.forward(3.1416)
+  t.left(3.6)
+t.left(90)
+for i in range(50):
+  t.forward(3.1416)
+  t.right(3.6)
+for i in range(75):
+  t.forward(3.1416)
+  t.left(3.6)
+for i in range(100):
+  t.forward(3.1416)
+  t.right(3.6)
+t.right(180)
+for i in range(75):
+  t.forward(3.1416)
+  t.right(3.6)
+for i in range(50):
+  t.forward(3.1416)
+  t.left(3.6)
+t.left(90)
+for i in range(50):
+  t.forward(3.1416)
+  t.right(3.6)
+for i in range(75):
+  t.forward(3.1416)
+  t.left(3.6)
+for i in range(100):
+  t.forward(3.1416)
+  t.right(3.6)
+t.right(180)
+for i in range(75):
+  t.forward(3.1416)
+  t.right(3.6)
+for i in range(50):
+  t.forward(3.1416)
+  t.left(3.6)
+t.left(90)
+for i in range(50):
+  t.forward(3.1416)
+  t.right(3.6)
+for i in range(75):
+  t.forward(3.1416)
+  t.left(3.6)
+for i in range(100):
+  t.forward(3.1416)
+  t.right(3.6)
+t.right(180)
+for i in range(75):
+  t.forward(3.1416)
+  t.right(3.6)
+for i in range(50):
+  t.forward(3.1416)
+  t.left(3.6)
+t.left(90)
